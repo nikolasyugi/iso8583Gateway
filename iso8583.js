ab339d8df29058439816353d16efcd8566840326
Nikolas <nikolas@mobile2you.com.br>
2019-04-25 16:57:44 -0300

    changed response, organized code and included ascii decoding

--- iso8583.js
+++ iso8583.js
@@ -2,7 +2,7 @@
 /*
 
 incoming buffer:
-010447101200F834040328E0800800000000000000201699001812345678900000000000000010000000000010003229681904110956450369353131313031323133313443203130303030303030303030303030303030303031040123339900181234567890D4912101000007001030303030303030383136313430303030303131313030303030303030303030303132330832383132313938303834300088564231383230313631303235313435333432313133345649313832303136313032353134353334323131333456453138323031363130323531343533343231313334564331383230313631303235313435333432313133340012573F463331332E4430313132
+47101200F834040328E0800800000000000000201699001812345678900000000000000010000000000010003229681904110956450369353131313031323133313443203130303030303030303030303030303030303031040123339900181234567890D4912101000007001030303030303030383136313430303030303131313030303030303030303030303132330832383132313938303834300088564231383230313631303235313435333432313133345649313832303136313032353134353334323131333456453138323031363130323531343533343231313334564331383230313631303235313435333432313133340012573F463331332E4430313132
 
 DECODING:
 
@@ -35,9 +35,15 @@ Field 22 -> 353131313031
 
 
 module.exports = (incomingBuffer) => {
-    testBuffer = Buffer.from("010447101200F834040328E0800800000000000000201699001812345678900000000000000010000000000010003229681904110956450369353131313031323133313443203130303030303030303030303030303030303031040123339900181234567890D4912101000007001030303030303030383136313430303030303131313030303030303030303030303132330832383132313938303834300088564231383230313631303235313435333432313133345649313832303136313032353134353334323131333456453138323031363130323531343533343231313334564331383230313631303235313435333432313133340012573F463331332E4430313132", "hex")
+    testBuffer = Buffer.from("00f247101200f834040308e080080000000000000020169900181234567890000000000000001000000000001000322968190411095645036935313131303132313331344320313030303030303030303030303030303030303104012330303030303030383136313430303030303131313030303030303030303030303132330832383132313938303834300088564231383230313631303235313435333432313133345649313832303136313032353134353334323131333456453138323031363130323531343533343231313334564331383230313631303235313435333432313133340012573f463331332e4430313132", "hex")
+    let fieldsLength = [0, 0, -1, 6, 12, 12, 0, 0, 0, 0, 0, 6, 12, 0, 4, 0, 0, 0, 0, 0, 0, 0, 12, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, 0, 0, 0, 0, 12, 0, 0, 0, 8, 15, -1, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -2, 0, -3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -3, 0, -3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -2, 0, 0, 0, 0, 0] //0 = not used, -1 = variable max 2 digits, -2 = variable max 3 digits, -3 = variable max 4 digits, this array is consistent with provided documentation
 
-    let fieldsLength = [0, 0, -1, 6, 12, 12, 0, 0, 0, 0, 0, 6, 12, 0, 4, 0, 0, 0, 0, 0, 0, 0, 12, 0, 0, 0, 0, 0, 0, 0, 0, -1, 4, 0, 0, 0, 0, 12, 0, 0, 0, 8, 15, -1, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -2, 0, -3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0] //0 = not used, -1 = variable max 2 digits, -2 = variable max 3 digits, -3 = variable max 4 digits
+    //Response test
+    // testBuffer = Buffer.from("00f24710121080000000040000000000000000000001000111111", "hex")
+    //bmap 1 8000000004000000
+    // let fieldsLength = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    let asciiFields = [22, 31, 37, 41, 42, 43, 49, 50, 61, 63, 123]
 
     // /****DEBUG FIELDS LENGTH ****/
     // /****DEBUG FIELDS LENGTH ****/
@@ -50,16 +56,26 @@ module.exports = (incomingBuffer) => {
     let buffer = incomingBuffer || testBuffer
 
     let msg = buffer.toString('hex')
+    console.log(`Message: ${msg}`)
+
     let decoded = {}
-    
     decoded["mti"] = msg.substring(8, 12)
-    
+
+    console.log('f834040308e08008')
+    console.log(msg.substring(12, 28))
+    console.log(parseInt(msg.substring(12, 28), 16))
+    console.log(parseInt('f834040308e08008', 16).toString(2))
+    console.log('1111100000110100000001000000001100001000111000001000000000001000')
+
+    /* Bit Mapping */
     decoded["firstBitmap"] = parseInt(msg.substring(12, 28), 16).toString(2)
     decoded["firstBitmap"] = decoded["firstBitmap"].length == 64 ? decoded["firstBitmap"] : `00${decoded["firstBitmap"]}`
-    
+
     decoded["secondBitmap"] = parseInt(msg.substring(28, 44), 16).toString(2)
     decoded["secondBitmap"] = decoded["secondBitmap"].length == 64 ? decoded["secondBitmap"] : `${'0'.repeat(64 - decoded["secondBitmap"].length)}${decoded["secondBitmap"]}`
-    
+    /* Bit Mapping */
+
+    /* Fields present in message */
     let fields = [];
     let i = 1
     for (const c of decoded["firstBitmap"]) {
@@ -75,34 +91,80 @@ module.exports = (incomingBuffer) => {
         }
         j++;
     }
-    
+    console.log(`Fields: ${fields}`)
+    /* Fields present in message */
+
+
+    /* Decoding */
     let payload = msg.substring(44, msg.length - 1) //8 (header) + 4 (mti) + 32(bitmaps) = 44
     let start = 0
     fields.forEach((field) => {
         if (fieldsLength[field] > 0) { //fixed length fields
-            
-            decoded[`field_${field}`] = payload.substr(start, fieldsLength[field])
-            start += fieldsLength[field]
+
+            if (asciiFields.includes(field)) { //ASCII Fields
+
+                decoded[`field_${field}`] = Buffer.from(payload.substr(start, fieldsLength[field] * 2), 'hex').toString()
+                start += fieldsLength[field] * 2
+
+            } else { //non ASCII Fields
+
+                decoded[`field_${field}`] = payload.substr(start, fieldsLength[field])
+                start += fieldsLength[field]
+            }
 
         } else if (fieldsLength[field] < 0) { //variable length fields
-            if (fieldsLength[field] == -1) { //variable max 2 digits
-                let length = parseInt(payload.substring(start, start + 2))
-                start = start + 2
-                decoded[`field_${field}`] = payload.substr(start, length)
-                start += length
-            } else if (fieldsLength[field] == -2) { //variable max 3 digits
-                let length = parseInt(payload.substring(start, start + 3))
-                start = start + 3
-                decoded[`field_${field}`] = payload.substr(start, length)
-                start += length
-            } else if (fieldsLength[field] == -3) { //variable max 4 digits
-                let length = parseInt(payload.substring(start, start + 4))
-                start = start + 4
-                decoded[`field_${field}`] = payload.substr(start, length)
-                start += length
+            if (asciiFields.includes(field)) { //ASCII Fields
+
+                if (fieldsLength[field] == -1) { //variable max 2 digits
+
+                    let length = parseInt(payload.substring(start, start + 2)) * 2
+
+                    start = start + 2
+                    decoded[`field_${field}`] = Buffer.from(payload.substr(start, length), 'hex').toString()
+                    start += length
+
+                } else if (fieldsLength[field] == -2) { //variable max 3 digits
+                    let length = parseInt(payload.substring(start, start + 3)) * 2
+                    start = start + 3
+                    decoded[`field_${field}`] = Buffer.from(payload.substr(start, length), 'hex').toString()
+                    start += length
+                } else if (fieldsLength[field] == -3) { //variable max 4 digits
+                    if (field == 123) {
+                        console.log(start)
+                    }
+
+                    let length = parseInt(payload.substring(start, start + 4)) * 2
+                    if (field == 123) {
+                        console.log(payload.substring(start, start + 4))
+                    }
+                    start = start + 4
+                    decoded[`field_${field}`] = Buffer.from(payload.substr(start, length), 'hex').toString()
+                    start += length
+                }
+
+            } else { //non ASCII Fields
+
+                if (fieldsLength[field] == -1) { //variable max 2 digits
+                    let length = parseInt(payload.substring(start, start + 2))
+                    start = start + 2
+                    decoded[`field_${field}`] = payload.substr(start, length)
+                    start += length
+                } else if (fieldsLength[field] == -2) { //variable max 3 digits
+                    let length = parseInt(payload.substring(start, start + 3))
+                    start = start + 3
+                    decoded[`field_${field}`] = payload.substr(start, length)
+                    start += length
+                } else if (fieldsLength[field] == -3) { //variable max 4 digits
+                    let length = parseInt(payload.substring(start, start + 4))
+                    start = start + 4
+                    decoded[`field_${field}`] = payload.substr(start, length)
+                    start += length
+                }
+
             }
         }
     })
+    /* Decoding */
 
     return decoded
 }
