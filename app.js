040b27b7f247c645c5e6648eb0638b68deec62b2
Nikolas <nikolas@mobile2you.com.br>
2019-04-26 15:50:50 -0300

    implemented confirm buy

--- app.js
+++ app.js
@@ -38,14 +38,15 @@ mongoose.connect(keys.dbUrl, { useNewUrlParser: true })
                 console.log("**************** Incoming data ****************")
 
                 let time = 0;
-                const maxTimeout = keys.maxTimeout
+                const qrCodeTimeout = Math.round(keys.maxTimeout / 2)
+                const confirmBuyTimeout = Math.round(keys.maxTimeout / 2)
 
                 const interval = setInterval(() => {
-                    mongoose.connection.db.collection('iso_messages').findOne({ stan: '322968' }, function (err, isoFound) {
+                    mongoose.connection.db.collection('iso_messages').findOne({ stan: decoded.field_11 }, function (err, isoFound) {
                         if (!err) {
                             //While not found wait until timeout
 
-                            if (time >= maxTimeout) {//TIMEOUT
+                            if (time >= qrCodeTimeout) {//TIMEOUT
                                 clearInterval(interval);
                                 console.log("**************** Timeout ****************")
                                 replyMsg = Buffer.from(changeMti("1210", data.toString('hex')), "hex")
@@ -58,9 +59,39 @@ mongoose.connect(keys.dbUrl, { useNewUrlParser: true })
                                 clearInterval(interval);
                                 if (matchIso(isoFound, decoded)) { //CORRECT ISO
                                     console.log("**************** Matched ISO ****************")
-                                    replyMsg = Buffer.from(changeMti("1210", data.toString('hex')), "hex")
-                                    console.log(replyMsg)
-                                    console.log("**************** Matched ISO ****************")
+
+                                    let buyTime = 0
+
+                                    mongoose.connection.db.collection('iso_messages').findOneAndUpdate({ stan: decoded.field_11 }, { $set: { confirmed_order: true } }, { upsert: true }, (err, doc) => {
+                                        if (err) return console.log(err)
+                                        const buyInterval = setInterval(() => {
+
+
+                                            mongoose.connection.db.collection('iso_messages').findOne({ stan: decoded.field_11, confirmed_order: true, buy_order: true }, function (err, isoFound) {
+                                                if (!err) {
+                                                    if (isoFound) {
+                                                        console.log("**************** Success ****************")
+                                                        replyMsg = Buffer.from(changeMti("1210", testMsg), "hex")
+                                                        console.log(replyMsg)
+                                                        console.log("**************** Success ****************")
+                                                        clearInterval(buyInterval);
+                                                    } else if (buyTime >= confirmBuyTimeout) {
+                                                        console.log("**************** Timeout ****************")
+                                                        replyMsg = Buffer.from(changeMti("1210", testMsg), "hex")
+                                                        console.log(replyMsg)
+                                                        console.log("**************** Timeout ****************")
+                                                        clearInterval(buyInterval);
+                                                    }
+                                                } else {
+                                                    console.log(err)
+                                                }
+
+                                            })
+                                            buyTime++;
+                                            console.log(confirmBuyTimeout - buyTime)
+                                        }, 1000)
+
+                                    });
 
                                 } else { //WRONG ISO
                                     console.log("**************** Didn't Match ISO ****************")
@@ -75,8 +106,8 @@ mongoose.connect(keys.dbUrl, { useNewUrlParser: true })
                         }
                     });
                     time++;
-                    console.log(maxTimeout - time)
-                }, 2000)
+                    console.log(qrCodeTimeout - time)
+                }, 1000)
 
                 console.log("***********************************************")
                 console.log("")
